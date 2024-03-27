9d21d6a1eca5624298bc59ac6bc47a8f48e7051c
NicolasKarla <114199995+NicolasKarla@users.noreply.github.com>
2024-03-27 20:35:04 -0300

    Add files via upload

---
+++ myhtml/src/main/java/com/demo/myhtml/MyhtmlApplication.java
@@ -0,0 +1,13 @@
+package com.demo.myhtml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhtmlApplication.class, args);
+	}
+
+}
